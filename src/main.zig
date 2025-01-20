188be9ce38c5fbfa5c019d7f51c3038767a81993
Pat Lown <patlown@pm.me>
2025-01-19 22:31:11 -0700

    working 3d circle from sphere, get_pixel is meh

--- src/main.zig
+++ src/main.zig
@@ -3,41 +3,48 @@ const expect = std.testing.expect;
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
-    // try stdout.print("ray tracer initialization...\n", .{});
-    // to print a text circle, we'll need to do the following
-    // will represent . for white pixels and x for black pixels
-    // we'll represent a picture as an array (?) of arrays filled with either character x or .
-    // let's start by just creating and printing an array
 
-    // var pixels: [64][48]u8 = undefined;
-
-    // & get address of pixels
-    // * means row is a pointer type
-    // for (&pixels) |*row| {
-    //     // this function sets all the elements of a memory region to elem.
-    //     @memset(row, 'x');
-    // }
-
-    const width: i32 = 100;
-    const height: i32 = 100;
-    const circle_radius: i32 = 5;
-
+    const width: f32 = 64;
+    const height: f32 = 48;
     const max_value: u16 = 255;
-
-    const circle_center_x: i32 = @divFloor(width, 2);
-    const circle_center_y: i32 = @divFloor(height, 2);
 
     try stdout.print("P3\n", .{});
     try stdout.print("{d} {d}\n", .{ width, height });
     try stdout.print("{d}\n", .{max_value});
 
-    var y: i32 = 0;
+    const camera = Vec3.new(0, 0, -20);
+    const focal_dist: f32 = 10;
+    const sphere_center = Vec3.new(0, 0, 0);
+    const sphere_radius: f32 = 5;
+    const screen_up = Vec3.new(0, 1, 0);
+    const screen_right = Vec3.new(1, 0, 0);
+
+    const view_direction = Vec3.new(0, 0, 1);
+
+    var y: f32 = 0;
     while (y < height) : (y += 1) {
-        var x: i32 = 0;
+        var x: f32 = 0;
         while (x < width) : (x += 1) {
-            const dx = x - circle_center_x;
-            const dy = y - circle_center_y;
-            if (dx * dx + dy * dy < circle_radius * circle_radius) {
+            const step = Vec3.new(x, y, 0);
+            if (x == 32 and y == 24) {
+                std.debug.print("print step: ", .{});
+                step.print();
+            }
+
+            const pixel = get_pixel(step, camera, view_direction, focal_dist, width, height, screen_up, screen_right);
+            if (x == 32 and y == 24) {
+                std.debug.print("print pixel: ", .{});
+                pixel.print();
+            }
+
+            const dir = get_ray_direction(pixel, camera);
+            if (x == 32 and y == 24) {
+                std.debug.print("print dir: ", .{});
+                dir.print();
+                std.debug.print("\n", .{});
+            }
+
+            if (sphere_intersection(camera, dir, sphere_radius, sphere_center)) {
                 try stdout.print("255 0 0\n", .{});
             } else {
                 try stdout.print("0 255 0\n", .{});
@@ -45,3 +52,71 @@ pub fn main() !void {
         }
     }
 }
+
+pub fn sphere_intersection(camera: Vec3, ray_direction: Vec3, sphere_radius: f32, sphere_center: Vec3) bool {
+    const r_squared = sphere_radius * sphere_radius;
+    const L = camera.subtract(sphere_center);
+
+    const a = ray_direction.dot(ray_direction); // Length squared of ray direction
+    const b = 2.0 * L.dot(ray_direction); // 2 times alignment of L with ray
+    const c = L.dot(L) - r_squared; // Length squared of L minus radius squared
+
+    // std.debug.print("print a:{d}, b:{d}, c:{d} ", .{ a, b, c });
+
+    // Compute discriminant
+    const discriminant = (b * b) - (4.0 * a * c);
+    // If discriminant is negative, no intersection
+    if (discriminant < 0) {
+        return false;
+    }
+    return true; // Intersection exists
+}
+
+pub fn get_ray_direction(pixel: Vec3, camera: Vec3) Vec3 {
+    return pixel.subtract(camera);
+}
+
+pub fn get_pixel(step: Vec3, camera: Vec3, view_direction: Vec3, focal_distance: f32, screen_width: f32, screen_height: f32, screen_up: Vec3, screen_right: Vec3) Vec3 {
+    const screen_midpoint = Vec3.add(camera, Vec3.apply_scalar(view_direction, focal_distance));
+
+    // Convert pixel coordinates to be centered around 0:
+    // First, move origin to center by subtracting half width/height
+    const centered_x = step.x - (screen_width / 2.0);
+    const centered_y = step.y - (screen_height / 2.0);
+
+    // Then scale to our desired world size, accounting for aspect ratio
+    const x_step = screen_right.apply_scalar(centered_x * (focal_distance / screen_width));
+    const y_step = screen_up.apply_scalar(centered_y * (focal_distance / screen_width));
+
+    return screen_midpoint.add(x_step.add(y_step));
+}
+
+const Vec3 = struct {
+    x: f32,
+    y: f32,
+    z: f32,
+
+    pub fn new(x: f32, y: f32, z: f32) Vec3 {
+        return Vec3{ .x = x, .y = y, .z = z };
+    }
+
+    pub fn apply_scalar(self: Vec3, s: f32) Vec3 {
+        return Vec3.new(self.x * s, self.y * s, self.z * s);
+    }
+
+    pub fn add(self: Vec3, other: Vec3) Vec3 {
+        return Vec3.new(self.x + other.x, self.y + other.y, self.z + other.z);
+    }
+
+    pub fn subtract(self: Vec3, other: Vec3) Vec3 {
+        return Vec3.new(self.x - other.x, self.y - other.y, self.z - other.z);
+    }
+
+    pub fn dot(self: Vec3, other: Vec3) f32 {
+        return self.x * other.x + self.y * other.y + self.z * other.z;
+    }
+
+    pub fn print(self: Vec3) void {
+        std.debug.print("Vec3(x: {d}, y: {d}, z: {d})\n", .{ self.x, self.y, self.z });
+    }
+};
